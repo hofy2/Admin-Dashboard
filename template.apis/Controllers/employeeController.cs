cc2fec56dd12450a2917af7099a1ef1e595d29d5
hofy2 <61843032+hofy2@users.noreply.github.com>
2023-01-08 20:45:20 +0200

    Add files via upload

---
+++ template.apis/Controllers/employeeController.cs
@@ -0,0 +1,264 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using template.bl.helper;
+using template.bl.Interface;
+using template.bl.models;
+using template.dal.entity;
+
+namespace template.apis.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class employeeController : ControllerBase
+    {
+        #region field
+        private readonly Iemployeerep employee;
+        private readonly IMapper mapper;
+
+        #endregion
+
+
+        #region con
+
+       public employeeController(Iemployeerep employee , IMapper mapper)
+        {
+            this.employee = employee;
+            this.mapper = mapper;
+        }
+
+
+        #endregion
+
+
+        #region apis
+
+        [HttpGet]
+        [Route("~/api/Getemployee")]
+
+        public IActionResult Getemployee()
+        {
+            try
+            {
+                var data = employee.get();
+
+                var model = mapper.Map<IEnumerable<employeevm>>(data);
+                return Ok(new apiresponse<IEnumerable<employeevm>>
+                {
+
+                    code = "200",
+                    status = "ok",
+                    message = "data retreved",
+                    data = model
+
+
+                });
+            } catch(Exception ex)
+            {
+                return NotFound(new apiresponse<string>
+                {
+
+                    code = "404",
+                    status = "not found",
+                    message = "data not found",
+                   error = ex.Message
+
+
+                });
+            }
+        }
+
+
+
+        [HttpGet]
+        [Route("~/api/Getemployeebyid/{id}")]
+        public IActionResult Getemployeebyid( int id)
+        {
+            try
+            {
+                var data = employee.getbyid(id);
+
+                var model = mapper.Map<employeevm>(data);
+                return Ok(new apiresponse<employeevm>
+                {
+
+                    code = "200",
+                    status = "ok",
+                    message = "data retreved",
+                    data = model
+
+
+                });
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new apiresponse<string>
+                {
+
+                    code = "404",
+                    status = "not found",
+                    message = "data not found",
+                    error = ex.Message
+
+
+                });
+            }
+        }
+
+
+
+        [HttpPost]
+        [Route("~/api/postemployee")]
+
+        public IActionResult postemployee(employeevm model)
+        {
+            try
+            {
+                if ( ModelState.IsValid)
+                {
+
+                    var data = mapper.Map<employee>(model);
+                    var result = employee.creat(data);
+
+                    return Ok(new apiresponse<employee>
+                    {
+
+                        code = "201",
+                        status = "saved",
+                        message = "data seved",
+                        data = result
+
+
+                    });
+                }
+
+
+                return BadRequest(new apiresponse<string>
+                {
+
+                    code = "402",
+                    status = "bad request",
+                    message = "no data saved",
+                    error = "bad request"
+
+
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new apiresponse<string>
+                {
+
+                    code = "402",
+                    status = "bad request",
+                    message = "no data saved",
+                    error = ex.Message
+
+
+                });
+            }
+        }
+
+
+        [HttpPut]
+        [Route("~/api/putemployee")]
+
+        public IActionResult putemployee(employeevm model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    var data = mapper.Map<employee>(model);
+                   employee.update(data);
+
+                    return Ok(new apiresponse<employeevm>
+                    {
+
+                        code = "201",
+                        status = "updated",
+                        message = "data updated",
+                        data = model
+
+
+                    });
+                }
+
+
+                return BadRequest(new apiresponse<string>
+                {
+
+                    code = "402",
+                    status = "bad request",
+                    message = "no data saved",
+                    error = "bad request"
+
+
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new apiresponse<string>
+                {
+
+                    code = "402",
+                    status = "bad request",
+                    message = "no data saved",
+                    error = ex.Message
+
+
+                });
+            }
+        }
+
+        [HttpDelete]
+        [Route("~/api/deleteemployee")]
+
+        public IActionResult deleteemployee(employeevm model)
+        {
+            try
+            {
+               
+
+                    var data = mapper.Map<employee>(model);
+                    employee.delete(data);
+
+                    return Ok(new apiresponse<employeevm>
+                    {
+
+                        code = "201",
+                        status = "deleted",
+                        message = "data deleted",
+                        data = model
+
+
+                    });
+                
+
+
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new apiresponse<string>
+                {
+
+                    code = "402",
+                    status = "bad request",
+                    message = "no data saved",
+                    error = ex.Message
+
+
+                });
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ template.apis/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using template.bl.Interface;
+using template.bl.models;
+using template.bl.reposatry;
+using template.dal.database;
+
+namespace template.apis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+
+
+
+
+            services.AddDbContextPool<templatecontext>(opts =>
+    opts.UseSqlServer(Configuration.GetConnectionString("templateConnection")));
+
+            services.AddAutoMapper(x => x.AddProfile(new domainprofile()));
+
+            services.AddScoped<Iemployeerep, employeerep>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
